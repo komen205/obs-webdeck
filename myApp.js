6514552d91ece1e9c2c559c097e97d921977d0cd
komen205 <59452877+komen205@users.noreply.github.com>
2021-08-15 16:16:21 +0100

    Connecting to OBS websockets and adding some events

--- myApp.js
+++ myApp.js
@@ -1,48 +1,75 @@
 const { menubar } = require("menubar");
 const exec = require("child_process").exec;
 const mb = menubar();
-var validUrl = require("valid-url");
-
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 1234;
+const OBSWebSocket = require("obs-websocket-js");
+const obs = new OBSWebSocket();
+obs
+  .connect({
+    address: "localhost:4444",
+    password: "123",
+  })
+  .then(() => {
+    console.log(`Success! We're connected & authenticated.`);
 
-function execute(command, callback) {
-  exec(command, (error, stdout, stderr) => {
-    callback(stdout);
-  });
-}
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+    return obs.send("GetSceneList");
+  })
+  .then((data) => {
+    console.log(`${data.scenes.length} Available Scenes!`);
+  
+    data.scenes.forEach((scene) => {
+      if (scene.name !== data.currentScene) {
+        console.log(
+          `Found a different scene! Switching to Scene: ${scene.name}`
+        );
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname+"/index.html");
-});
-app.get("/chrome", (req, res) => {
-  res.sendFile(__dirname+"/chrome.html");
-});
-
-app.post("/chrome", (req, res) => {
-  if (validUrl.isUri(req.body.link)) {
-    execute('start "" "' + req.body.link + '"', (output) => {
-      res.send(output);
+        obs.send("SetCurrentScene", {
+          "scene-name": scene.name,
+        });
+      }
     });
-  } else {
-    res.send("error");
-  }
+  })
+  .catch((err) => {
+    // Promise convention dicates you have a catch on every chain.
+    console.log(err);
+  });
+
+  
+obs.on("StartStreaming", (data) => {
+  console.log(`Started streaming`);
 });
 
-app.get("/shutdown", (req, res) => {
-  execute("shutdown -s", (output) => {
-    res.send(output);
+obs.on("StopStreaming", (data) => {
+  console.log(`Stopped streaming`);
+});
+
+
+obs.on("SwitchScenes", (data) => {
+  console.log(`New Active Scene: ${data.sceneName}`);
+});
+
+// You must add this handler to avoid uncaught exceptions.
+obs.on("error", (err) => {
+  console.error("socket error:", err);
+});
+
+app.get("/switch", (req, res) => {
+  obs.send("SetCurrentScene", {
+    "scene-name": req.query.scene,
   });
+  res.send('done');
+});
+
+app.get("/start", (req, res) => {
+  obs.send("StartStreaming");
 });
 
 app.get("/stop", (req, res) => {
-  execute("shutdown -a", (output) => {
-    res.send(output);
-  });
+  obs.send("StopStreaming");
 });
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
