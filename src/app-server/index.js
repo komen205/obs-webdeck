a37e6bef7fd1b5062f89ccbbf79b44ed4dcaf876
Joao Patricio <ijpatricio@gmail.com>
2021-09-03 16:46:00 +0100

    wip

--- src/app-server/index.js
+++ src/app-server/index.js
@@ -1,16 +1,23 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const path = require('path');
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const path = require('path')
+const { Server } = require('socket.io')
+const io = new Server(server)
 
 const mobileAppPath = __dirname + '/../src/app-mobile/dist'
-app.use(express.static(mobileAppPath));
+app.use(express.static(mobileAppPath))
 
 app.get('/', function (req,res) {
-    res.sendFile(path.resolve(mobileAppPath+'/index.html'));
-  }); 
+    res.sendFile(path.resolve(mobileAppPath+'/index.html'))
+  })
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+const port = 3000
+server.listen(port, () => {
+  console.log('listening on *:' + port)
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
