ca6c9ad2818c03e2a8a9c358baea922785debf2c
Joao Patricio <ijpatricio@gmail.com>
2021-09-03 21:25:49 +0100

    Adds Tailwind 2 to desktop app

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './app-desktop/App.vue'
+import "tailwindcss/tailwind.css"
 
 Vue.config.productionTip = false
 
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    './src/app-desktop/**/*.vue',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
